66256d7ce53d3072f38ba246aa8a04b14f196350
Felipe Couto <feliperafaeldocouto@hotmail.com>
2023-08-06 20:24:25 -0300

    Servidor Alocado no Express

--- src/script/rota.js
+++ src/script/rota.js
@@ -1,10 +1,21 @@
+//Importando o FramemeWork Express
 const express = require('express')
 const app = express();
 
+//Se URL for /, enviaram o Homepage)
 app.get('/', (res, res)=> {
     res.send('home page')
 })
 
+//Rota com parametros (Caso use alguem hora)
+
+//app.get('/usuario/:name', (req, res)=> {
+ // const name = req.params.name;
+ // res.send(req.params) 
+//})
+
+//Definindo a Rota do servidor
 app.listen(3000, () => {
     console.log('Servidor Em execução em http://localhost:3000');
 })
+
