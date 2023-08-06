07efe565b3d81e417861d48f0df3cf25043f2b6e
Felipe Couto <feliperafaeldocouto@hotmail.com>
2023-08-06 17:42:34 -0300

    Update index.js

--- src/script/index.js
+++ src/script/index.js
@@ -1,16 +1,22 @@
 const http = require('http'); //Colocando HTTP no servidor
+const http = require('fs');
+
 
 const server = http.createServer((req, res) => {
-
-   res.setHeader('Content-Type', 'text/plain');
-   if (req.url === '/') {
-        res.statusCode = 200; // Status OK 
-        res.end('Home page'); //Escreve Home Page 
-    } else (req.url === '/about');  {
-        res.statusCode = 200;
-        res.end('About')
-    }
-});
+    if (req.url === '/') {
+         fs.readFile('index.html', (err, data) => {
+             if (err) {
+                 res.statusCode = 500; // Erro interno do servidor
+                 res.end('Erro ao ler o arquivo');
+             } else {
+                 res.setHeader('Content-Type', 'text/html');
+                 res.statusCode = 200;
+                 res.end(data); // Envia o conteúdo do index.html como resposta
+             }
+         });
+        }
+    });
+    
 
 server.listen(3000, () => {
  console.log(`Servidor em execução em http://localhost:3000/`);
