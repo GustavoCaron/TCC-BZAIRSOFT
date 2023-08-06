8d79ef52fdd2fdec84c10eef0ab8ec1801db572e
Felipe Couto <feliperafaeldocouto@hotmail.com>
2023-08-06 19:59:08 -0300

    Update rota.js

--- src/script/rota.js
+++ src/script/rota.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express();
 
+app.get('/', (res, res)=> {
+    res.send('home page')
+})
+
 app.listen(3000, () => {
     console.log('Servidor Em execução em http://localhost:3000');
 })
